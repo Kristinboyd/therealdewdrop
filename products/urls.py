8e25c4c80ffdc3f6e96e40282431b199093dc612
micksumm <65046453+micksumm@users.noreply.github.com>
2021-08-09 14:39:44 -0700

    Rename route for listing all products

--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,8 @@ app_name = 'products'
 
 urlpatterns = [
     path('<int:pk>/', ProductDetail.as_view(), name='detailcreate'),
-    path('', CreateProduct.as_view(), name='listcreate'),
-    path('list', ListProducts.as_view(), name='listproducts'),
+    path('add-product', CreateProduct.as_view(), name='listcreate'),
+    path('', ListProducts.as_view(), name='listproducts'),
     re_path(r'^add-to-profile/(?P<username>\w{0,50})/$', add_to_profile, name='add_to_profile'),
     re_path(r'^filter/(?P<username>\w{0,50})/$', ProductGetByCondition.as_view())
 ]
