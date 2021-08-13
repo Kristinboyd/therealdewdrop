f85afadecdc2a405b08aa35f49a57c1b73749c39
Kristin <Kristineliseboyd13@gmail.com>
2021-08-13 12:16:39 -0700

    debug to false

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ["therealdewdrop.herokuapp.com"]
 
